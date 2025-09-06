1e7ce6d9d2dab252b85d3e3f6b02a4c287653a29
08edu02 <86580343+08edu02@users.noreply.github.com>
2025-09-06 01:09:00 -0500

    feat: URLS

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# REMOTE URL: https://redesneuronalesproyectofinal-w4qgu7dk2bgacptqfbobug.streamlit.app/
+# LOCAL: http://localhost:8501/#
+
 import streamlit as st
 import pandas as pd
 import numpy as np
